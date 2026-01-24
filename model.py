6d8bc2a0d30baa7c2cb4d3fbcb2abcb6cfed5193
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-25 04:21:14 +0530

    Initial commit: Add project files

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels,
+                               3, stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels,
+                               3, stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        self.use_shortcut = stride != 1 or in_channels != out_channels
+        if self.use_shortcut:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1,
+                          stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
